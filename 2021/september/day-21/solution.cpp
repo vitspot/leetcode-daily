634cf512f3932d880afb1805874b2a4249bc0e92
Mukka Deepak <ramu92013@gmail.com>
2021-09-21 12:45:45 +0530

    solution: 2021 / september / 21 / cpp / 1

---
+++ 2021/september/day-21/solution.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        int maxCount = INT_MIN;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i])
+                count++;
+            else
+                count = 0;
+            if (count > maxCount)
+            {
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
+};
